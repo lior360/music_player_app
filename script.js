04cb743860ff9d0f4196d779456fdb3f9e901db0
Lior Shalev <liorshalev6@gmail.com>
2022-05-01 22:26:33 +0300

    Prev and Next function

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+
+
+const image = document.querySelector('img')
+const title = document.querySelector('#title');
+const artist = document.querySelector('#artist');
 const music = document.querySelector("audio");
 const prevBtn = document.querySelector('#prev');
 const playBtn = document.querySelector('#play');
 const nextBtn = document.querySelector('#next');
+
+//music
+const songs=[
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Front Row',
+        artist: 'Jacinto Design'
+    }
+];
 
 //Check if playing
 let isPlaying = false;
@@ -27,3 +51,41 @@ function pauseSong(){
 playBtn.addEventListener('click',()=>{
     isPlaying? pauseSong() : playSong();
 })
+
+//update DOM
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist ;
+    music.src =`music/${song.name}.mp3`;
+    image.src= `img/${song.name}.jpg`;
+}
+
+//current song
+let songIndex = 0;
+
+//Previous song
+function prevSong(){
+    songIndex --;
+    if (songIndex<0){
+        songIndex = songs.length-1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+//next song
+function nextSong(){
+    songIndex = (songIndex+1)%(songs.length);
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//On Load
+loadSong(songs[songIndex]);
+
+
+//event Listener
+
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
